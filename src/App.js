7e15f2edb35297e93bbac498c492932fe258e928
jjjliu1995 <46224518+jjjliu1995@users.noreply.github.com>
2020-02-01 16:24:03 -0500

    changing navigation pane code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import React from 'react';
-import Toolbar from './components/Toolbar/Toolbar';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {Link} from 'react-router-dom'
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <Toolbar />
-      <main style={{marginTop: '64px'}}>
-        <p>This is the page content</p>
-      </main> 
-    </div>
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="static" style={{backgroundColor: 'green'}}>
+          <Toolbar >
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Home
+            </Typography>
+            <Link to='/user'>
+              <Button color="inherit">User</Button>
+            </Link>
+            <Link to='/donate'>
+              <Button color="inherit">Donate</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div style={{height:'100%'}}>
+//       <Toolbar />
+//       <SideDrawer />
+//       <main style={{marginTop: '64px'}}>
+//         <p>This is the page content</p>
+//       </main> 
+//     </div>
+//   );
+// }
+
+// export default App;
