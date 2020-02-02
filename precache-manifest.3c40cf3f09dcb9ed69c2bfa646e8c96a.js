fc7abd8e71d61597d8c9171b44507dfa5ba851c5
jjjliu1995 <46224518+jjjliu1995@users.noreply.github.com>
2020-02-02 10:34:39 -0500

    Updates

---
+++ precache-manifest.3c40cf3f09dcb9ed69c2bfa646e8c96a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7895883184cbd7d67481ceb45410c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d557765cf25f06c5d68",
+    "url": "/static/css/main.c6f28c0c.chunk.css"
+  },
+  {
+    "revision": "b827255c683ad171d359",
+    "url": "/static/js/2.4dfe85fc.chunk.js"
+  },
+  {
+    "revision": "01614b5008b8342eab2dca154aea9eb9",
+    "url": "/static/js/2.4dfe85fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d557765cf25f06c5d68",
+    "url": "/static/js/main.a58489b7.chunk.js"
+  },
+  {
+    "revision": "570f31e47be24d6680db",
+    "url": "/static/js/runtime-main.b5d42951.js"
+  },
+  {
+    "revision": "b46aec8178446c3f340f26d65a0e6700",
+    "url": "/static/media/climatechange.b46aec81.jpg"
+  },
+  {
+    "revision": "d16ae289ad5ace152fecfd5405b5472e",
+    "url": "/static/media/ellehack.d16ae289.jpg"
+  },
+  {
+    "revision": "a9ff4c6dc5e8f67f11bebe2af018da87",
+    "url": "/static/media/tedx.a9ff4c6d.jpg"
+  },
+  {
+    "revision": "04a317cdfcd3e69f5d967c1f39cac5a5",
+    "url": "/static/media/us2u_logo_green.04a317cd.png"
+  }
+]);
